22f97dfb0b8568c152aecfaeaa3b6a8d785777fd
Atila Rajkov <atilar777@gmail.com>
2019-06-22 11:30:54 +0200

    Implemented "clean-blog" template.

---
+++ resources/views/blog/list-articles.blade.php
@@ -0,0 +1,20 @@
+@extends('blog.layout.main')
+
+@section('seo-title')
+<title>List Articles</title>
+@endsection
+
+@section('custom-css')
+
+@endsection
+
+
+@section('banner-image')
+<header class="masthead" style="background-image: url('https://cdna.artstation.com/p/assets/images/images/017/729/070/large/anato-finnstark-sekiro-the-deep-forest-by-anatofinnstark-dd6adi0-fullview.jpg?1557136943')">
+@endsection
+
+
+@section('heading-and-subheading')
+<h1>All articles</h1>
+<span class="subheading">Sekiro. The deep forest</span>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Custom routes
+Route::get('/list-articles', 'ArticlesController@index')->name('list_articles');
